f6d0b6fbfc4c23ea510b12e1527423700e397d87
Treeways <56928485+Treeways@users.noreply.github.com>
2025-07-19 23:06:28 -0500

    Fix broken link and cleanup

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,8 +6,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Archipelago Minecraft Rewrite',
-  tagline: '',
-  // favicon: 'img/favicon.ico',
+  tagline: 'A modular, extensible and flexible randomizer for Minecraft Archipelago.',
+  favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/overview">
+            Read Documentation
           </Link>
         </div>
       </div>
@@ -36,9 +36,9 @@ export default function Home(): ReactNode {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
+      {/* <main>
         <HomepageFeatures />
-      </main>
+      </main> */}
     </Layout>
   );
 }
