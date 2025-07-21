471e5bbc80e3c17008d951acbaca3eddee67dd69
Treeways <56928485+Treeways@users.noreply.github.com>
2025-07-21 05:55:52 -0500

    Update the page title and tweak homepage

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Archipelago Minecraft Rewrite',
-  tagline: 'A modular, extensible and flexible randomizer for Minecraft Archipelago.',
+  tagline: 'A modular, extensible and flexible Minecraft randomizer for Archipelago.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
